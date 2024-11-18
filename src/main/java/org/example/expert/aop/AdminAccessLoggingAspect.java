edf33132aa865354fcd6030896c593eef9d9b36e
jangms3 <github.com/jangms3>
2024-11-18 20:58:52 +0900

    test : todo_ 단건 조회 시 todo 가 존재하지 않아 예외가 발생한다.
    -mockMvc 의 기대 상태는 isOK() 로 설정되어 있음
    -Todo not found 예외가 발생한다고 가정하고 있어 논리적 불일치가 발생.
    -또한 404 를 기대하고 있는데 400을 반환하는 오류 발생으로 인해
    -domain 에 InvalidRequestException을 추가하였고 GlobalExceptionHandler 에서 handleInvalidRequestExceptionException 를 통해 컨트롤 할 수 있도록 기대하였음.
    - but 왜인지 모르겠지만 실행이 여전히 안됨.

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -1,11 +1,13 @@
 package org.example.expert.aop;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -18,9 +20,13 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController..ChangeUserRole(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
-        String userId = String.valueOf(request.getAttribute("userId"));
+        //사용자 id 가져오기
+        String userId = request.getAttribute("userId")!= null
+            ? String.valueOf(request.getAttribute("userId"))
+            :"unknown";
+        //요청 정보 가져오기
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
 
